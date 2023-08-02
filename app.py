889ab82cdbb4b9a70af049d83145380d7b0ef1c9
jordanrburger <jordanrburger@gmail.com>
2023-08-02 16:23:38 -0400

    removing our logo :(

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from sqlalchemy.dialects import registry
 registry.load("snowflake")
 
 st.set_page_config(page_title="Kai SQL Bot Demo", page_icon=":robot:")
-st.image('static/keboola_logo.png', width=400)
+#st.image('static/keboola_logo.png', width=400)
 st.header("Kai SQL Bot Demo ")
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
