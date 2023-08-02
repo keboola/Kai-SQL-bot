bce6c4fd04f5aac02cc5273a416ac5a14cdd8315
jordanrburger <jordanrburger@gmail.com>
2023-08-02 16:01:17 -0400

    adding ability to connect to other DBs

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import AgentExecutor
 import os
 
-from src.workspace_connection.workspace_connection import connect_to_snowflake
-from src.keboola_storage_api.connection import _add_connection_form
+from src.workspace_connection.workspace_connection import connect_to_snowflake, snowflake_connection_user_input
+from src.keboola_storage_api.connection import _add_connection_form, add_keboola_table_selection
 from sqlalchemy.dialects import registry
 registry.load("snowflake")
 
@@ -21,16 +21,25 @@ st.header("Kai SQL Bot Demo ")
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-account_identifier = st.secrets["account_identifier"]
-user = st.secrets["user"]
-password = st.secrets["password"]
-database_name = st.secrets["database_name"]
-schema_name = st.secrets["schema_name"]
-warehouse_name = st.secrets["warehouse_name"]
-role_name = st.secrets["role_name"]
+conn_method = st.selectbox("Connection Method", ["Demo Database", "Snowflake Database Connection"])
 
-conn_string = f"snowflake://{user}:{password}@{account_identifier}/{database_name}/{schema_name}?warehouse={warehouse_name}&role={role_name}"
-db = SQLDatabase.from_uri(conn_string)
+
+if conn_method == "Snowflake Database Connection":
+    connect_to_snowflake()
+    conn_string = f"snowflake://{st.session_state['user']}:{st.session_state['password']}@{st.session_state['account']}/{st.session_state['database']}/{st.session_state['schema']}?warehouse={st.session_state['warehouse']}&role={st.session_state['user']}"
+    db = SQLDatabase.from_uri(conn_string)
+else:
+    st.write("Using Demo Database") 
+    account_identifier = st.secrets["account_identifier"]
+    user = st.secrets["user"]
+    password = st.secrets["password"]
+    database_name = st.secrets["database_name"]
+    schema_name = st.secrets["schema_name"]
+    warehouse_name = st.secrets["warehouse_name"]
+    role_name = st.secrets["role_name"]
+    conn_string = f"snowflake://{user}:{password}@{account_identifier}/{database_name}/{schema_name}?warehouse={warehouse_name}&role={role_name}"
+    db = SQLDatabase.from_uri(conn_string)
+
 toolkit = SQLDatabaseToolkit(llm=ChatOpenAI(model='gpt-3.5-turbo-16k', temperature=0), db=db)
 
 
