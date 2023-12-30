f24deaa57e143be6bfa2dbd0058bca41f61bb3ea
jordanrburger <jordanrburger@gmail.com>
2023-12-30 11:04:39 -0500

    Update database connections and add evaluation functionality

--- validation.py
+++ validation.py
@@ -1,31 +1,95 @@
 import openai
 import streamlit as st
-import os
-
+import pandas as pd
 import json
+import datetime
+import concurrent.futures
 
-
-from langchain.memory import StreamlitChatMessageHistory
 from langchain.memory import ConversationBufferMemory
-from langchain.memory.chat_message_histories import StreamlitChatMessageHistory 
-
-from langchain.agents import create_sql_agent, AgentExecutor
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.sql_database import SQLDatabase
 from langchain.chat_models import ChatOpenAI
-from langchain.agents.agent_types import AgentType
-from langchain.prompts import PromptTemplate
 from langchain.evaluation import load_evaluator
-from src.workspace_connection.workspace_connection import initialize_connection
-from agent import create_agent
+from langchain.callbacks import LLMonitorCallbackHandler
+
+from src.database_connection.database_connection import DatabaseConnection
+from agent import SQLAgentCreator
 from few_shot_examples import custom_tool_list
+from prompts import custom_gen_sql
+
+#get current datetime as a unix timestamp
+current_time = datetime.datetime.now().timestamp()
+
+lunary_callback = LLMonitorCallbackHandler(app_id=st.secrets.LUNARY_APP_ID)
+lunary_user_id = f"ValidationRun-{current_time}"
 
 memory = ConversationBufferMemory()
 
-conn_string = initialize_connection()
-
 llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0,streaming=True)
 
-agent_executor = create_agent(conn_string=conn_string, llm=llm, custom_tool_list=custom_tool_list, memory=memory)
+db_conn = DatabaseConnection(st.secrets["shopify_ecommerce_db"]["account_identifier"], st.secrets["shopify_ecommerce_db"]["user"], st.secrets["shopify_ecommerce_db"]["password"],
+                    st.secrets["shopify_ecommerce_db"]["database_name"], st.secrets["shopify_ecommerce_db"]["schema_name"], st.secrets["shopify_ecommerce_db"]["warehouse_name"], st.secrets["shopify_ecommerce_db"]["role_name"])
 
+toolkit = db_conn.create_toolkit(llm)
 
+agent_creator = SQLAgentCreator(toolkit=toolkit, llm=llm, custom_tool_list=custom_tool_list, memory=memory)
+
+evaluator = load_evaluator("labeled_pairwise_string")
+with open('validation.json', 'r') as f:
+    data = json.load(f)
+
+print("data loaded")
+
+n = 0
+evaluation_output = {}
+
+def process_data(item):
+    agent_executor = agent_creator.create_agent()
+    n = item[0]
+    data = item[1]
+    print(f"Question: {data['question']}")
+    prompt_formatted = custom_gen_sql.format(context=data['question'])
+
+    try:
+        response = agent_executor.run(input=prompt_formatted, memory=memory, callbacks=[lunary_callback], metadata={ "agentName": "KaiSQLBot", "user_id": lunary_user_id})
+    except ValueError as e:
+        response = str(e)
+        if not response.startswith("Could not parse LLM output: `"):
+            raise e
+        response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
+    except openai.InvalidRequestError as e:
+        response = str(e)
+        if response.startswith("InvalidRequestError: This model's maximum context length%"):
+            response = "Model context length exceeded. Please try again."
+
+    evaluation = evaluator.evaluate_string_pairs(
+        prediction=response, # new agent run
+        prediction_b=data['answer'], # baseline
+        input=data['question'],
+        reference=data['answer']
+    )
+    print(f"Baseline: {data['answer']}")
+    print(f"New Output: {response}")
+    print(evaluation)
+
+    return {
+        "n": n,
+        "model": 'GPT-4-Turbo',
+        "agent_type": 'OPENAI_FUNCTIONS',
+        "question": data['question'],
+        "baseline_answer": data['answer'],
+        "prediction (new agent run)": response,
+        "evaluation": evaluation
+    }
+    
+
+evaluation_output = {}
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = executor.map(process_data, enumerate(data))
+
+    for result in results:
+        evaluation_output[result["n"]] = result
+
+print("evaluation complete")
+
+df = pd.DataFrame.from_dict(evaluation_output, orient='index')
+
+df.to_csv('evaluation_output.csv', index=False)
