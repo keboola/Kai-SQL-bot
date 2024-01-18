7d42980f39b3fba2f67a7aea4f7bc22fa5e9acdb
jordanrburger <jordanrburger@gmail.com>
2024-01-18 18:58:01 -0500

    Add new chat display functionality

--- chat.py
+++ chat.py
@@ -1,9 +1,29 @@
 import streamlit as st
-from prompts import ai_intro, custom_gen_sql
+from prompts import ai_intro, custom_gen_sql, pandy_gen_sql
 from langchain.memory import StreamlitChatMessageHistory, ConversationBufferMemory
-from langchain.callbacks import StreamlitCallbackHandler, LLMonitorCallbackHandler, FileCallbackHandler
+from langchain.callbacks import FileCallbackHandler
+from langchain_community.callbacks import LLMonitorCallbackHandler
+from langchain_community.callbacks import StreamlitCallbackHandler
+
 
 class ChatDisplay:
+    """
+    Represents a chat display for interacting with an AI agent.
+
+    Args:
+        agent_executor (AgentExecutor): An instance of the AgentExecutor class responsible for executing AI agent actions.
+        memory (Memory): An instance of the Memory class representing the memory of the AI agent.
+
+    Attributes:
+        agent_executor (AgentExecutor): An instance of the AgentExecutor class responsible for executing AI agent actions.
+        memory (Memory): An instance of the Memory class representing the memory of the AI agent.
+        msgs (StreamlitChatMessageHistory): An instance of the StreamlitChatMessageHistory class representing the chat message history.
+
+    Methods:
+        display_chat(): Displays the chat messages and handles user input.
+
+    """
+
     def __init__(self, agent_executor, memory):
         self.agent_executor = agent_executor
         self.memory = memory
@@ -11,8 +31,8 @@ class ChatDisplay:
 
     def display_chat(self):
         st_callback = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
-        lunary_callback = LLMonitorCallbackHandler(app_id=st.secrets.LUNARY_APP_ID)
-        logfile_handler = FileCallbackHandler("chat_log.txt")
+        #lunary_callback = LLMonitorCallbackHandler(app_id=st.secrets.LUNARY_APP_ID)
+        #logfile_handler = FileCallbackHandler("chat_log.txt")
 
         if len(self.msgs.messages) == 0:
             self.msgs.add_ai_message(ai_intro)
@@ -24,18 +44,20 @@ class ChatDisplay:
             self.msgs.add_user_message(prompt)
             st.chat_message("user").write(prompt)    
             
-            prompt_formatted = custom_gen_sql.format(context=prompt)
+            prompt_formatted = pandy_gen_sql.format(context=prompt)
             try:
-                response = self.agent_executor.run(input=prompt_formatted, callbacks=[st_callback, lunary_callback, logfile_handler], memory=self.memory, return_intermediate_steps=True)
+                response = self.agent_executor.invoke({"input": prompt_formatted})
+                                                #, callbacks=[st_callback, #lunary_callback, 
+                                                # logfile_handler], memory=self.memory, return_intermediate_steps=True)
             except ValueError as e:
                 response = str(e)
                 if not response.startswith("Could not parse LLM output: `"):
                     raise e
                 response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
         
-            self.msgs.add_ai_message(response)
-            st.chat_message("Kai").write(response)
-
+            self.msgs.add_ai_message(response["output"])
+            st.chat_message("ai").write(response["output"])
+            
 # Usage
 # agent_executor = ... # Initialize your agent executor
 # memory = ... # Initialize your memory
